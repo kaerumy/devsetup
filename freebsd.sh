2f1f4d1cb5822f2ec05ca1882dd20cebba34f132
Khairil Yusof <kaeru@inigo-tech.com>
2015-02-19 22:44:03 +0800

    Update freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -1,5 +1,4 @@
 #python, plone, development
-pkg add vim python27 the_silver_searcher libxml2 libxslt git subversion
-
+pkg add vim python27 the_silver_searcher libxml2 libxslt git subversion py-virtualenv
 #golang
 pkg add go
